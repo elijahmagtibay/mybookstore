f52947441cfd1e9dcf6ddc0970190c74f118d147
11399791 <11399791@TAFT-CL321>
2017-09-15 14:10:42 +0800

    my first commit

---
+++ MyBookstore/App_Code/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Configuration;
+
+namespace MyBookstore.App_Code
+{
+    public class Helper
+    {
+        public static string GetCon()
+        {
+            return ConfigurationManager.ConnectionStrings["MyCon"].ConnectionString; 
+       
+        }
+    }
+}
